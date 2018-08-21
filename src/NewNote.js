8339a7e23a390c6ff30aa5b636d71475b0194920
pkopy <pawel.kopycki@gmail.com>
2018-08-21 20:34:51 +0200

    installing form-serialize

--- src/NewNote.js
+++ src/NewNote.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class NewNote extends Component {
   render () {
     return (
       <div>
-        XXXXXXXXX
+        <Link className="close-new-note-button" to="/">Close</Link>
+        <form className="create-new-note-form">
+          <div className="new-note-details">
+            <input type="text" name="title" placeholder="Title"/>
+            <input type="text" name="content" placeholder="content"/>
+            <input type="checkbox" name="done" value="false"/>
+            <input type="date" name="dline"/>
+            <button>Send</button>
+          </div>
+        
+        </form>
       </div>
     )
   }
