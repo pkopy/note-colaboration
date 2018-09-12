678e44c0f7391d7a4fe2515f3f56226505ec237b
kopycki <pawel.kopycki@gmail.com>
2018-09-12 15:52:51 +0200

    testing loader

--- src/Login.js
+++ src/Login.js
@@ -8,6 +8,8 @@ class Login extends Component {
   loginUser = (e) => {
     e.preventDefault();
     const values = serializeForm(e.target, { hash: true})
+    const x = document.querySelector('.oko')
+        x.style.backgroundColor ='#000'
     // fetch('http://40.115.96.228:8080/login',{
     fetch('http://localhost:8080/login',{
         method: 'POST',
@@ -15,7 +17,11 @@ class Login extends Component {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(values)
-      }).then(res => res.json())
+        
+      }).then(res => {
+        console.log('poszło')
+        
+       return res.json()})
       .then((data) => {
         console.log(data.notes)
         if(data.login){
@@ -24,6 +30,8 @@ class Login extends Component {
           this.props.addAllNotes(data.notes)
           // this.setState({user:data})
         }
+        const x = document.querySelector('.oko')
+        x.style.backgroundColor ='#fff'
         console.log(data)
         
       })
@@ -54,6 +62,7 @@ class Login extends Component {
         </form>)
         
         }
+        <div className="oko">XXXXXXXXXXXXXXXX</div>
       </div>
     )
   }
